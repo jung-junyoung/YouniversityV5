9d282dc4b68c47b27eb7f35ebb72fb40689421c2
jjyyuunn <jjyyuunn@yonsei.ac.kr>
2025-04-22 16:15:27 +0900

    initial setting + footer

---
+++ web/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <body>
+        
+      </body>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ web/src/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-message">
+        Be the YOU in YOUN!VERSITY. Spot it. Report it. Improve it. - for everyone!
+      </div>
+      <div className="footer-contact">Contact: youniversity@gmail.com</div>
+      <div className="footer-copyright">
+        Copyright 2025. ⓒ YOUN!VERSITY. All rights reserved.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
